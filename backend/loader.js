a87a3e5a8a2c4e872489eebce8536165a9d5a7e8
Douglas Bollella <Douglas Bollella>
2017-03-12 22:30:07 -0300

    route

---
+++ backend/loader.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+const server = require('./config/server')
+require('./config/database')
+require('./config/routes')(server)
+=======
+require('./config/server')
+require('./config/database')
+>>>>>>> ece5789d7721275259e07ca9f92a3c1ffa763d7c
